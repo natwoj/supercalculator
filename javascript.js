dc96610f185380752da9a48c56c4708a682c2101
_natwoj <info@nwojcik.com>
2021-06-28 19:19:39 +0200

    supercalculator

---
+++ javascript.js
@@ -0,0 +1,11 @@
+//click
+document.getElementById="wyslij".addEventListener("click", wynikDziałania);
+
+function wynikDzialania () {
+	var mojOperator;
+  var wynik;
+
+//pull number or string
+var operand1 = document.getElementById("operand1").value;
+var operand2 = document.getElementById("operand2").value;
+
